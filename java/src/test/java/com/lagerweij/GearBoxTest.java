585a95eb6d134594fd7497bc7480a43d4096b5c0
christopher <christopher.zelezny@mailbox.org>
2020-12-21 16:47:25 +0100

    test switch gear up and down

--- java/src/test/java/com/lagerweij/GearBoxTest.java
+++ java/src/test/java/com/lagerweij/GearBoxTest.java
@@ -15,18 +15,26 @@ public class GearBoxTest {
     }
 
     @Test
-    public void should_not_switch_in_range_rpm() {
+    public void should_switch_from_first_to_second_gear() {
         GearBox gearBox = new GearBox();
-        Assert.assertTrue(true);
+        gearBox.switchGear(1000);
+        gearBox.switchGear(2001);
+        assertEquals(2,gearBox.getGear());
     }
 
-
-
+    @Test
+    public void should_switch_from_second_to_third_gear() {
+        GearBox gearBox = new GearBox();
+        gearBox.switchGear(1000);
+        gearBox.switchGear(2001);
+        gearBox.switchGear(499);
+        assertEquals(1,gearBox.getGear());
+    }
 
     /*
     *   Testfälle:
-    *   +Wir sind im ersten Gang, haben 1000 Umdrehungen und sind immer noch im ersten Gang
-    *   +Sind im ersten Gang mit 2000 Umdrehungen und gehen in den zweiten Gang
+    *   +Wir sind im ersten Gang, haben 1000 Umdrehungen und sind immer noch im ersten Gang _
+    *   +Sind im ersten Gang mit 2000 Umdrehungen und gehen in den zweiten Gang _
     *   +Sind im zweiten Gang, haben 500 Umdrehungen und sind im ersten Gang
     *   +
     * */
